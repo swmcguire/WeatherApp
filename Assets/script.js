38808e29e1b64a8b3ab1e8b04fbcb3c65d63a00c
swmcguire <swmcguire1@comcast.net>
2022-12-05 16:36:28 -0600

    Got all the fetches to update the correct data

--- Assets/script.js
+++ Assets/script.js
@@ -1,70 +1,111 @@
-var cityName = "minneapolis";
-var atlanta = "lat=33.7489924&lon=-84.3902644"; 
-var chicago = "lat=41.8755616&lon=-87.6244212"; 
-var dallas = "lat=32.7762719&lon=-96.7968559";
-var denver = "lat=39.7392364&lon=-104.984862";
-var minneapolis = "lat=44.9772995&lon=-93.2654692";
-var newYork = "lat=40.7127281&lon=-74.0060152";
-var stPaul = "lat=44.9497487&lon=-93.0931028";
-var lat = "";
-var lon = "";
-var latLon = "" ;
-//var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=minneapolis&limit=1&appid=a8b255a467db2ae7a256c9499b3b3509';
-var geoURL = "\'http://api.openweathermap.org/geo/1.0/direct?q\=" + cityName + "\&limit=1&appid=a8b255a467db2ae7a256c9499b3b3509\'";
 
-console.log(geoURL);
+  //-----------------------------------------------------GEO-LOCATER-----------------------------------------------------//
+  
+    var cityName = 'minneapolis';
+    var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q\=' + cityName + '&limit=1&appid=a8b255a467db2ae7a256c9499b3b3509';
 
-  //--------------------------GEO-LOCATER---------------------------------//
-  //fetch('http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=1&appid=a8b255a467db2ae7a256c9499b3b3509')
-  function getGeoCode(geoURL){
-  fetch(geoURL)
+    fetch(geoURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    //console.log(data);
     lat = (data[0].lat);
     lon = (data[0].lon);
     latLon="lat=" + lat + "&lon=" + lon;
     console.log(lat);
     console.log(lon);
     console.log(latLon);
-  
+    return latLon;
   });
-}
-getGeoCode(geoURL);
 
 
-//--------------------------CURRENT WEATHER---------------------------------//
-//fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=a8b255a467db2ae7a256c9499b3b3509&units=imperial')
-//----------------------------Need to update lat=xxxxxx&lon=xxxxx with the latLon variable above
-fetch('https://api.openweathermap.org/data/2.5/weather?lat=45.0313156&lon=-93.3395843&appid=a8b255a467db2ae7a256c9499b3b3509&units=imperial')
+ //-----------------------------------------------------CURRENT WEATHER-----------------------------------------------------//
+
+var latLon = 'lat=44.9772995&lon=-93.2654692';
+var currentWeather = 'https://api.openweathermap.org/data/2.5/weather?' + latLon + '&appid=a8b255a467db2ae7a256c9499b3b3509&units=imperial';
+
+fetch(currentWeather)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    //console.log(data);
+    temp = (data.main.temp) + " F";
+    wind = (data.wind.speed) + " MPH";
+    humidity = (data.main.humidity) + " %";
+    console.log(temp);
+    console.log(wind);
+    console.log(humidity);
+
   });
 
-  
+ 
 
-//--------------------------5 Day/ 3Hr WEATHER---------------------------------//
-//fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=a8b255a467db2ae7a256c9499b3b3509&units=imperial')
-//----------------------------Need to update lat=xxxxxx&lon=xxxxx with the latLon variable above
-fetch('https://api.openweathermap.org/data/2.5/forecast?lat=45.0313156&lon=-93.3395843&appid=a8b255a467db2ae7a256c9499b3b3509&units=imperial')
+ //-----------------------------------------------------5 Day/ 3Hr WEATHER-----------------------------------------------------//
+
+var latLon = 'lat=44.9772995&lon=-93.2654692';
+var futureWeather = 'https://api.openweathermap.org/data/2.5/forecast?' + latLon + '&appid=a8b255a467db2ae7a256c9499b3b3509&units=imperial';
+
+fetch(futureWeather)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    //console.log(data);
+    date1 = (data.list[4].dt_txt);
+    temp1 = (data.list[4].main.temp) + " F";
+    wind1 =  (data.list[4].wind.speed)+ " MPH";
+    humidity1 = (data.list[4].main.humidity) + " %";
+    date2 = (data.list[12].dt_txt);
+    temp2 = (data.list[12].main.temp) + " F";
+    wind2 =  (data.list[12].wind.speed)+ " MPH";
+    humidity2 = (data.list[12].main.humidity) + " %";
+    date3 =  (data.list[20].dt_txt);
+    temp3 = (data.list[20].main.temp) + " F";
+    wind3 =  (data.list[20].wind.speed)+ " MPH";
+    humidity3 = (data.list[20].main.humidity) + " %";
+    date4 =  (data.list[28].dt_txt);
+    temp4 = (data.list[28].main.temp) + " F";
+    wind4 =  (data.list[28].wind.speed)+ " MPH";
+    humidity4 = (data.list[28].main.humidity) + " %";
+    date5 = (data.list[36].dt_txt);
+    temp5 = (data.list[36].main.temp) + " F";
+    wind5 =  (data.list[36].wind.speed)+ " MPH";
+    humidity5 = (data.list[36].main.humidity) + " %";
+
+    console.log(date1);
+    console.log(temp1);
+    console.log(wind1);
+    console.log(humidity1);
+    console.log(date2);
+    console.log(temp2);
+    console.log(wind2);
+    console.log(humidity2);
+    console.log(date3);
+    console.log(temp3);
+    console.log(wind3);
+    console.log(humidity3);
+    console.log(date4);
+    console.log(temp4);
+    console.log(wind4);
+    console.log(humidity4);
+    console.log(date5);
+    console.log(temp5);
+    console.log(wind5);
+    console.log(humidity5);
+
   });
 
-  console.log(chicago);
 
 
-  /* When I enter a city name and click 'search' --- the variable gets stored in cityName 
+
+  /*
+  --When I enter a city name and click 'search' --- the variable gets stored in cityName 
   --The GeoLocator then uses that variable to call the API to get latitude and longitude.
   --That latitude and longitued get passed to the current weather and 5 day weather API
   --That data gets called to the screen 
 
   --when I click on a preselected city button, that city name populates cityName variable and runs the same as above
+
+  */
